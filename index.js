2ba7bc6a91ef9772843157bea4cdfdfa13d2250a
Eriberto Guzman <eriberto176@gmail.com>
2020-04-04 07:47:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,3 +17,23 @@ function divide(a,b){
   a /= b 
   return a
 }
+
+function increment(n){
+  n++
+  return n
+}
+
+function decrement(n){
+  n--
+  return n
+}
+
+function makeInt(n){
+  parseInt(n, 10)
+  return n
+}
+
+function preserveDecimal(n){
+  parseFloat(n)
+  return n
+}
