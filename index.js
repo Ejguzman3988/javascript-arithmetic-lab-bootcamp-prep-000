020409bcb2de0ec1c21e7df0f8cad583df61f80c
Eriberto Guzman <eriberto176@gmail.com>
2020-04-04 07:42:28 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,19 @@
+function add(a,b){
+  c = a+b
+  return c
+}
+
+function subtract(a,b){
+  c = a-b
+  return c
+}
+
+function multiple(a,b){
+  c = a*b
+  return c
+}
+
+function divde(a,b){
+  c = a/b 
+  return c
+}
